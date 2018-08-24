de2b4209bbff236001a2648fd2819ef07281567a
Christian Zosel <christian.zosel@adfinis-sygroup.ch>
2018-08-24 09:21:58 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+model.exports = {
+  rules: {
+    // best practices
+    "array-callback-return": 2,
+    "dot-notation": 2,
+    eqeqeq: 2,
+    "no-alert": 2,
+    "no-else-return": 2,
+    "no-eval": 2,
+    "no-extend-native": 2,
+    "no-extra-bind": 2,
+    "no-floating-decimal": 2
+  }
+};
